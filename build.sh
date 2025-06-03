38fb01377d424a6c6e310ad0bc1611dd839b95a3
CodedBeard <codedbeard@noreply.users.github.com>
2025-06-03 19:14:12 +0100

    comment out single thread build/install commands as they are only used on low power devices

--- build.sh
+++ build.sh
@@ -2,12 +2,14 @@
 # sudo apt install -y libcurl4-openssl-dev libavcodec-dev libavdevice-dev libopencv-dev
 # meson setup build -Denable_libav=enabled -Denable_drm=enabled -Denable_egl=disabled -Denable_qt=disabled -Denable_opencv=enabled -Denable_tflite=disabled -Denable_hailo=disabled -Denable_imx500=true -Ddownload_imx500_models=true --reconfigure
 # Run the build command
-ninja -C build -j 1
+#ninja -C build -j 1
+ninja -C build
 
 # Check if --install is passed as an argument
 if [[ "$1" == "--install" ]]; then
     # Run the install command with sudo
-    sudo ninja -C build install -j 1
+    #sudo ninja -C build install -j 1
+    sudo ninja -C build install
     sudo ldconfig
 fi
 
