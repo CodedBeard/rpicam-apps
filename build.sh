f8e5cf4c2091fc27e886f715ad1927915ea6c739
CodedBeard <codedbeard@users.noreply.github.com>
2025-11-23 22:59:41 +0000

    split setup and build scripts

--- build.sh
+++ build.sh
@@ -1,6 +1,3 @@
-#!/bin/bash
-# sudo apt install -y libcurl4-openssl-dev libavcodec-dev libavdevice-dev libopencv-dev
-# meson setup build -Denable_libav=enabled -Denable_drm=enabled -Denable_egl=disabled -Denable_qt=disabled -Denable_opencv=enabled -Denable_tflite=disabled -Denable_hailo=disabled -Denable_imx500=true -Ddownload_imx500_models=true --reconfigure
 # Run the build command
 #ninja -C build -j 1
 ninja -C build
